a4d8ac84d710e01c935a1834d89dd7fad8b9fa16
Lukasrygh23 <Lukas@jazzcatz.com>
2018-12-10 11:59:09 +0000

    Quickly fixed some last things. Made more points.

--- coursework/src/main/webapp/WEB-INF/pages/csvjs.js
+++ coursework/src/main/webapp/WEB-INF/pages/csvjs.js
@@ -30,13 +30,13 @@ function processData(allText) {
 function initMap() {
   // Creating the map and marking where it sets itself up.
   var london = {lat: 51.5072, lng: 0.1272};
-  var londonArray = [{lat: 51.485723, lng: -0.173176}, {lat: 51.499102, lng: -0.160395}, {lat: 51.494815, lng: -0.185346}];
+  var londonArray = [{lat: 51.485723, lng: -0.173176}, {lat: 51.499102, lng: -0.160395}, {lat: 51.494815, lng: -0.185346}, {lat:51.494815, lng:-0.185346}, {lat:51.520947, lng:-0.212271}, {lat: 51.491043, lng:-0.197308}, {lat:51.496484 , lng:-0.206169}, {lat:51.482636 , lng:-0.171283}, {lat:51.509766 , lng:-0.192678}, {lat:51.491227 , lng:-0.180159}, {lat:51.485812 , lng:-0.150127}, {lat:51.489269, lng:-0.164104}, {lat:51.480775 , lng:-0.184607}, {lat:51.495710 , lng:-0.173497}, {lat:51.481027 , lng:-0.183445}, ];
   //These represent the first three pieces of data, by the way.
   // The map, centered on London
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 12, center: london});
   // Plus a quick marker. 
-  for(count = 0; count < 3; count++){
+  for(count = 0; count < londonArray.length; count++){
                //Does a bucket of points.
 			   var marker = new google.maps.Marker({position: londonArray[count], map: map});
             }
